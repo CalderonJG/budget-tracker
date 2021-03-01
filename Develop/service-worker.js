766fbe663ec706b2110279bbaadb28f2adfbba84
Chimera <j.calderon3@gmail.com>
2021-03-01 17:24:51 -0500

    webpack script created

--- Develop/service-worker.js
+++ Develop/service-worker.js
@@ -5,7 +5,7 @@ const FILES_TO_CACHE = [
     '/style.css',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png',
-    '/dist/bundle.js',
+    '/manifest.webmanifest',
 ];
 
 const STATIC_CACHE = "static-cache-v2";
