3a856a7e3986414f87192e58dc332120626ae9cd
Chimera <j.calderon3@gmail.com>
2021-03-03 10:52:19 -0500

    webpack updated

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,28 @@
 const path = require('path');
 
 const config = {
-
+    entry: {
+        app: './public/index.js',
+    },
+    output: {
+        path: __dirname + '/dist',
+        filename: '[name].bundle.js',
+    },
+    mode: 'development',
+    module: {
+        rules: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env'],
+                },
+            },
+        },
+        ],
+    },
 };
 
 module.exports = config;
